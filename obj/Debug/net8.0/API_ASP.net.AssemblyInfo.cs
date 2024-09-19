3fffcf876973765d66815f919552638b4ed60765
Rusecode <usoficialemail@gmail.com>
2024-09-19 00:44:21 -0300

    .

--- obj/Debug/net8.0/API_ASP.net.AssemblyInfo.cs
+++ obj/Debug/net8.0/API_ASP.net.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_ASP.net")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d8f769876bd10718aea743016dcb3c4f2962db4")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_ASP.net")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_ASP.net")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
